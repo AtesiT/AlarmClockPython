68c02c448434196217c3be3f04582b964682c6cf
AtesiT <atesit@mail.ru>
2023-03-13 20:31:09 +0300

    alarmclock was did it

---
+++ file.py
@@ -0,0 +1,29 @@
+from playsound import playsound
+import time
+
+CLEAR = '\033[2J'
+CLEAR_AND_RETURN = '\033[H'
+
+def alarm(seconds):
+    #Прошло времени
+    time_elapsed = 0
+
+    print(CLEAR)
+    while time_elapsed < seconds:
+        time.sleep(1)
+        time_elapsed += 1
+        
+        time_left = seconds - time_elapsed
+        # Делим на минуты
+        minutes_left = time_left // 60
+        # Остаются секунды
+        seconds_left = time_left % 60
+
+        print(f'{CLEAR_AND_RETURN}Время до сигнала: {minutes_left:02d}:{seconds_left:02d}')
+
+    playsound('alarm.mp3')
+
+minutes = int(input('Введите минуты, через которое должен сработать таймер: ', ))
+seconds = int(input('Введите секунды, через которое должен сработать таймер: ', ))
+total_seconds = minutes * 60 + seconds
+alarm(total_seconds)
